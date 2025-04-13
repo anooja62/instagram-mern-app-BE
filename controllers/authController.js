7cc621cbcec00f95c3a5f3b91e3de598708cf643
Anooja M <anoojam@jabirs-MacBook-Air.local>
2025-04-13 13:47:44 +0530

    urls changed

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,8 @@ const { getInstagramAccessToken } = require('../services/instagramService');
 
 const instagramLogin = (req, res) => {
   const { INSTAGRAM_CLIENT_ID, INSTAGRAM_REDIRECT_URI } = process.env;
-  const redirectUrl = `https://api.instagram.com/oauth/authorize?client_id=${INSTAGRAM_CLIENT_ID}&redirect_uri=${INSTAGRAM_REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
+  //const redirectUrl = `https://api.instagram.com/oauth/authorize?client_id=${INSTAGRAM_CLIENT_ID}&redirect_uri=${INSTAGRAM_REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
+  const redirectUrl = "https://instagram-mern-app-fe.vercel.app/dashboard";
   res.redirect(redirectUrl);
 };
 
@@ -11,7 +12,8 @@ const instagramCallback = async (req, res) => {
     try {
       const tokenData = await getInstagramAccessToken(code);
       // Redirect to frontend with token and user ID
-      const frontendUrl = `https://instagram-mern-app-fe.vercel.app/dashboard?access_token=${tokenData.access_token}&user_id=${tokenData.user_id}`;
+     // const frontendUrl = `https://instagram-mern-app-fe.vercel.app/dashboard?access_token=${tokenData.access_token}&user_id=${tokenData.user_id}`;
+     const frontendUrl = "https://instagram-mern-app-fe.vercel.app/dashboard";
       res.redirect(frontendUrl);
     } catch (error) {
       console.error('OAuth error:', error);
